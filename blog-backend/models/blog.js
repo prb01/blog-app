a73b4abc355674af4f70a7a581634a9ac90f27e4
prb01 <p.bergstroem@gmail.com>
2022-06-02 12:05:40 -0500

    Task 13.18.

--- blog-backend/models/blog.js
+++ blog-backend/models/blog.js
@@ -10,6 +10,13 @@ Blog.init(
     url: { type: DataTypes.TEXT, allowNull: false },
     title: { type: DataTypes.TEXT, allowNull: false },
     likes: { type: DataTypes.INTEGER, defaultValue: 0 },
+    year: {
+      type: DataTypes.INTEGER,
+      validate: {
+        min: 1991,
+        max: new Date().getFullYear(),
+      },
+    },
   },
   { sequelize, underscored: true, timestamps: true, modelName: "blog" }
 )
